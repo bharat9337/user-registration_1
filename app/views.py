0d59a08a1dbaa69fe4a9b92f00bc72fcb16151c8
bharatjena123 <bharatjena2021@gmail.com>
2024-01-24 18:02:48 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login
 
 
 def registration(request):
@@ -21,14 +24,61 @@ def registration(request):
         if ufd.is_valid() and pfd.is_valid():
             MUFDO=ufd.save(commit=False)
             pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
             MUFDO.save()
 
             MPFDO=pfd.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
+
+            send_mail('registration','Hii this is Bharat','bharatjena2021@gmail.com',
+                      [MUFDO.email],fail_silently=False)
+
+
+
             return HttpResponse('Registration done')
         else:
             return HttpResponse('Invalid data')
 
     return render(request,'registration.html',d)
+
+
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render (request,'home.html',d)
+    
+
+    return render(request,'home.html')
+
+
+
+
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+        
+    return render(request,'user_login.html')
+
+
+
+
+
+
+
     
--- project35/settings.py
+++ project35/settings.py
@@ -128,3 +128,12 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='93378900482023b@gmail.com'
+EMAIL_HOST_PASSWORD='yttl oiog efza ehvk'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
